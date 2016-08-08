483c91bfec5228712141ab23407a89d4c92d2d2f
Leif_Chen <silver78821@gmail.com>
2016-08-08 10:53:39 +0000

    V1版存款完成

--- index.php
+++ index.php
@@ -28,16 +28,88 @@ class Payment
     // ----------------------------------------
     // 提(出)款
     // ----------------------------------------
-    function dispensingMoney()
+    function dispensingMoney($money)
     {
         
     }
     // ----------------------------------------
     // 存(入)款
     // ----------------------------------------
-    function depositMoney()
+    function depositMoney($money)
     {
+        // ----------------------------------------
+        // 定義使用者
+        // ----------------------------------------
         
+        $ID = 'Leif_Chen';
+        
+        // ----------------------------------------
+        
+        $db = new PDO("mysql:host=localhost;dbname=PayMent", "root", "");
+        $db->exec("SET CHARACTER SET utf8");
+        $db->beginTransaction();
+        
+        $eventList = "SELECT `totalAssets` FROM `MemberData` WHERE `MemberName` = :ID FOR UPDATE;" ;
+        $prepare = $db->prepare($eventList);
+        $prepare->bindParam(':ID', $ID);
+        $prepare->execute();
+        $result = $prepare->fetchAll(PDO::FETCH_ASSOC);
+        $nowMoney = $result[0]["totalAssets"];
+        
+        $totalMoney = $nowMoney + $money;
+        
+        // ----------------------------------------
+        // 更新會員資料
+        // ----------------------------------------
+        
+        $eventList = "UPDATE `MemberData` SET 
+                    `totalAssets` = :totalMoney 
+                    WHERE `MemberName` = :ID" ; 
+        $prepare = $db->prepare($eventList);
+        $prepare->bindParam(':totalMoney', $totalMoney);
+        $prepare->bindParam(':ID', $ID);
+        $prepare->execute();
+        
+        // ----------------------------------------
+        // 更新動作明細
+        // ----------------------------------------
+        
+        date_default_timezone_set('Asia/Taipei');
+        $time = date("Y-m-d H:i:s") ;
+        $action = 0 ;
+        
+        $eventList ="INSERT INTO `TransactionDetails` (
+                            `MemberName` ,
+							`dateTime` ,
+							`preTotalAssets` ,
+							`action` ,
+							`money` ,
+							`afterTotalAssets`
+							) 
+						VALUES ( 
+						    :ID ,
+							:time ,
+							:preTotalAssets ,
+							:action ,
+							:money ,
+							:afterTotalAssets
+			            	)"; 
+            
+        $prepare = $db->prepare($eventList);
+        $prepare->bindParam(':ID', $ID);
+        $prepare->bindParam(':time', $time);
+        $prepare->bindParam(':preTotalAssets', $nowMoney);
+        $prepare->bindParam(':action', $action);
+        $prepare->bindParam(':money', $money);
+        $prepare->bindParam(':afterTotalAssets', $totalMoney);
+        $prepare->execute();
+        
+        // ----------------------------------------  
+        
+        echo "<script language='JavaScript'>";
+        echo "alert('存款完成');location.href='/_payment/';";
+        echo "</script>";
+        $db->commit();
     }
 }
 
@@ -47,13 +119,11 @@ $basicData = $MemberData->takeMemberData();
 
 if (isset($_POST["btnDispensing"])) 
 {
-    $money = $_POST["txtMoneyCount"];
-    $MemberData->dispensingMoney($money);
+    $MemberData->dispensingMoney($_POST["txtMoneyCount"]);
 }
 if (isset($_POST["btnDeposit"])) 
 {
-    $money = $_POST["txtMoneyCount"];
-    $MemberData->depositMoney($money);
+    $MemberData->depositMoney($_POST["txtMoneyCount"]);
 }
 
 ?>
@@ -69,11 +139,11 @@ if (isset($_POST["btnDeposit"]))
         餘額 : <?php echo $List["totalAssets"]; ?><br>
         <br>
         <?php endforeach ?>
-        <form>
+        <form id = "formcreate" name = "formcreate" method = "post">
             執行動作 : 
             <input type="text" name="txtMoneyCount" id="txtMoneyCount"><br><br>
-            <input type = "button" name = "btnDispensing" id = "btnDispensing" value = "出款">
-            &nbsp;<input type = "button" name = "btnDeposit" id = "btnDeposit" value = "入款"><br>
+            <input type = "submit" name = "btnDispensing" id = "btnDispensing" value = "出款">
+            &nbsp;<input type = "submit" name = "btnDeposit" id = "btnDeposit" value = "入款"><br>
         </form>
         <br>
         明細 : <br>
