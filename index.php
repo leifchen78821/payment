5e580075ae0e04458ebcba905c1009a458c9246b
Leif_Chen <silver78821@gmail.com>
2016-08-08 08:59:54 +0000

    V1版顯示個人資料

--- index.php
+++ index.php
@@ -1,5 +1,35 @@
 <?php
 
+class Payment 
+{
+    // ----------------------------------------
+    // 取得基本資料
+    // ----------------------------------------
+    function takeMemberData()
+    {
+        // ----------------------------------------
+        // 定義使用者
+        // ----------------------------------------
+        
+        $ID = 'Leif_Chen';
+        
+        // ----------------------------------------
+        
+        $db = new PDO("mysql:host=localhost;dbname=PayMent", "root", "");
+        $db->exec("SET CHARACTER SET utf8");
+        
+        $eventList = "SELECT * FROM `MemberData` WHERE `MemberName` = :ID ;" ;
+        $prepare = $db->prepare($eventList);
+        $prepare->bindParam(':ID',$ID);
+        $prepare->execute();
+        $result = $prepare->fetchAll(PDO::FETCH_ASSOC);
+        return $result ;
+    }
+}
+
+$MemberData = new Payment();
+$basicData = $MemberData->takeMemberData();
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -7,10 +37,12 @@
         <meta http-equiv = "Content-Type" content = "text/html ; charset = UTF-8">
     </head>
     <body>
-        帳號 : <br>
+        <?php foreach($basicData as $List): ?>
+        帳號 : <?php echo $List["MemberName"]; ?><br>
         <br>
-        餘額 : <br>
+        餘額 : <?php echo $List["totalAssets"]; ?><br>
         <br>
+        <?php endforeach ?>
         <form>
             執行動作 : 
             <input type="text" name="txtMoneyCount" id="txtMoneyCount"><br><br>
