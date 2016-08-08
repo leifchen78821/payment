d301ceb64602d985c28eb1ccf4e30d1904b2146b
Leif_Chen <silver78821@gmail.com>
2016-08-08 07:03:13 +0000

    初版顯示介面完成

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv = "Content-Type" content = "text/html ; charset = UTF-8">
+    </head>
+    <body>
+        帳號 : <br>
+        <br>
+        餘額 : <br>
+        <br>
+        <form>
+            <input type = "button" name = "btnDispensing" id = "btnDispensing" value = "出款">
+            <input type = "button" name = "btnDeposit" id = "btnDeposit" value = "入款"><br>
+        </form>
+        <br>
+        明細 : <br>
+        <br>
+        <table border = "1" width = "500px">
+            <tr>
+            <td width = "35%">時間</td>
+            <td width = "15%">執行動作</td>
+            <td width = "25%">金額</td>
+            <td width = "25%">餘額</td>
+            </tr>
+        </table>
+    </body>
+</html>
