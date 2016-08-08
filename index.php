75fc4da7b74d52f355d8fccba37dacef43543d5d
Leif_Chen <silver78821@gmail.com>
2016-08-08 11:23:45 +0000

    V1版出款完成

--- index.php
+++ index.php
@@ -53,7 +53,91 @@ class Payment
     // ----------------------------------------
     function dispensingMoney($money)
     {
+        // ----------------------------------------
+        // 定義使用者
+        // ----------------------------------------
         
+        $ID = 'Leif_Chen';
+        
+        // ----------------------------------------
+        
+        $db = new PDO("mysql:host=localhost;dbname=PayMent", "root", "");
+        $db->exec("SET CHARACTER SET utf8");
+        $db->beginTransaction();
+        
+        $eventList = "SELECT `totalAssets` FROM `MemberData` WHERE `MemberName` = :ID FOR UPDATE;" ;
+        $prepare = $db->prepare($eventList);
+        $prepare->bindParam(':ID', $ID);
+        $prepare->execute();
+        $result = $prepare->fetchAll(PDO::FETCH_ASSOC);
+        $nowMoney = $result[0]["totalAssets"];
+        
+        // $totalMoney = $nowMoney + $money;
+        
+        if($nowMoney >= $money)
+        {
+            // ----------------------------------------
+            // 更新會員資料
+            // ----------------------------------------
+            
+            $totalMoney = $nowMoney - $money;
+            
+            $eventList = "UPDATE `MemberData` SET 
+                        `totalAssets` = :totalMoney 
+                        WHERE `MemberName` = :ID" ; 
+            $prepare = $db->prepare($eventList);
+            $prepare->bindParam(':totalMoney', $totalMoney);
+            $prepare->bindParam(':ID', $ID);
+            $prepare->execute();
+            
+            // ----------------------------------------
+            // 更新動作明細
+            // ----------------------------------------
+            
+            date_default_timezone_set('Asia/Taipei');
+            $time = date("Y-m-d H:i:s") ;
+            $action = 0 ;
+            
+            $eventList = "INSERT INTO `TransactionDetails` (
+                                `MemberName` ,
+    							`dateTime` ,
+    							`preTotalAssets` ,
+    							`action` ,
+    							`money` ,
+    							`afterTotalAssets`
+    							) 
+    						VALUES ( 
+    						    :ID ,
+    							:time ,
+    							:preTotalAssets ,
+    							:action ,
+    							:money ,
+    							:afterTotalAssets
+    			            	)"; 
+                
+            $prepare = $db->prepare($eventList);
+            $prepare->bindParam(':ID', $ID);
+            $prepare->bindParam(':time', $time);
+            $prepare->bindParam(':preTotalAssets', $nowMoney);
+            $prepare->bindParam(':action', $action);
+            $prepare->bindParam(':money', $money);
+            $prepare->bindParam(':afterTotalAssets', $totalMoney);
+            $prepare->execute();
+            
+            // ----------------------------------------  
+            
+            echo "<script language='JavaScript'>";
+            echo "alert('出款完成');location.href='/_payment/';";
+            echo "</script>";
+            $db->commit();
+        }
+        else
+        {
+            echo "<script language='JavaScript'>";
+            echo "alert('出款失敗');location.href='/_payment/';";
+            echo "</script>";
+            $db->rollback();
+        }
     }
     // ----------------------------------------
     // 存(入)款
@@ -130,7 +214,7 @@ class Payment
         // ----------------------------------------  
         
         echo "<script language='JavaScript'>";
-        echo "alert('存款完成');location.href='/_payment/';";
+        echo "alert('入款完成');location.href='/_payment/';";
         echo "</script>";
         $db->commit();
     }
