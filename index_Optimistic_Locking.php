41087b499da879fe517e4195b743e250270564f9
Leif_Chen <silver78821@gmail.com>
2016-08-11 02:46:42 +0000

    修改 index_Optimistic_Locking.php 例外處理

--- index_Optimistic_Locking.php
+++ index_Optimistic_Locking.php
@@ -124,7 +124,7 @@ class Payment
     {
         try {
             $this->db->beginTransaction();
-            $sql = "SELECT `totalAssets`, `numberTicket` FROM `MemberData` WHERE `memberName` = :id FOR UPDATE;";
+            $sql = "SELECT `totalAssets`, `numberTicket` FROM `MemberData` WHERE `memberName` = :id ;";
             $prepare = $this->db->prepare($sql);
             $prepare->bindParam(':id', $this->id);
             $prepare->execute();
@@ -146,6 +146,19 @@ class Payment
                 $prepare->bindParam(':id', $this->id);
                 $prepare->bindParam(':numberTicket', $nowNumberTicket);
                 $prepare->execute();
+
+                // ----------------------------------------
+                // 比較號碼牌，後來的人會出錯
+                // ----------------------------------------
+                $sql = "SELECT `numberTicket` FROM `MemberData` WHERE `memberName` = :id ;";
+                $prepare = $this->db->prepare($sql);
+                $prepare->bindParam(':id', $this->id);
+                $prepare->execute();
+                $result = $prepare->fetch(PDO::FETCH_ASSOC);
+                $sqlNumberTicket = $result["numberTicket"];
+                if($sqlNumberTicket != $nowNumberTicket + 1) {
+                    throw new Exception("有人比你早進來呦!!無法執行");
+                }
 
                 // ----------------------------------------
                 // 更新動作明細
@@ -176,10 +189,11 @@ class Payment
                 echo "alert('出款失敗');location.href='/_payment/index_Optimistic_Locking.php?member=" . $_GET["member"] . "';";
                 echo "</script>";
             }
+
             $this->db->commit();
         } catch (Exception $err) {
             echo "<script language='JavaScript'>";
-            echo "alert('" . $err . "');location.href='/_payment/index_Optimistic_Locking.php?member=" . $_GET["member"] . "';";
+            echo "alert('" . $err->getMessage() . "');location.href='/_payment/index_Optimistic_Locking.php?member=" . $_GET["member"] . "';";
             echo "</script>";
             $this->db->rollback();
         }
