450ae0b4f4c7fd08d90d274c6607492855bd6a18
Leif_Chen <silver78821@gmail.com>
2016-08-12 09:50:43 +0000

    單元測試 新增setup teardown

--- _testPayment/myProject/Test/PessimisticLockingTest.php
+++ _testPayment/myProject/Test/PessimisticLockingTest.php
@@ -4,12 +4,30 @@ require_once "myProject/IndexPessimisticLocking.php";
 
 class PessimisticLockingTest extends \PHPUnit_Framework_TestCase
 {
+    public $db = null;
+    public $id = null;
+
+    protected function setUp()
+    {
+        $this->id = 'Lid_Chen';
+        $this->db = new PDO("mysql:host=localhost;dbname=PayMent", "root", "");
+        $this->db->exec("SET CHARACTER SET utf8");
+
+        $sql = "INSERT INTO `MemberData` " .
+            "(`memberName`, `totalAssets`, `numberTicket`)" .
+            "VALUES " .
+            "('Lid_Chen' , '10000', '1');";
+        $prepare = $this->db->prepare($sql);
+        $prepare->execute();
+    }
+
     // 測試新增會員
     public function testAddNewMemberSuccess() {
         $payment = new Payment();
         $newMemberName = 'Anny';
         $expectedResult = "新增使用者 : Anny 成功";
         $result = $payment->addNewMember($newMemberName);
+
         $this->assertEquals($expectedResult, $result);
     }
 
@@ -18,6 +36,7 @@ class PessimisticLockingTest extends \PHPUnit_Framework_TestCase
         $payment = new Payment();
         $expectedResult = 490000;
         $result = $payment->takeMemberData();
+
         $this->assertEquals($expectedResult, $result);
     }
 
@@ -27,6 +46,7 @@ class PessimisticLockingTest extends \PHPUnit_Framework_TestCase
         $money = 1000;
         $expectedResult = "出款完成";
         $result = $payment->dispensingMoney($money);
+
         $this->assertEquals($expectedResult, $result);
     }
 
@@ -36,6 +56,7 @@ class PessimisticLockingTest extends \PHPUnit_Framework_TestCase
         $money = 99999999999999;
         $expectedResult = "出款失敗";
         $result = $payment->dispensingMoney($money);
+
         $this->assertEquals($expectedResult, $result);
     }
 
@@ -45,7 +66,18 @@ class PessimisticLockingTest extends \PHPUnit_Framework_TestCase
         $money = 10000;
         $expectedResult = "入款完成";
         $result = $payment->depositMoney($money);
+
         $this->assertEquals($expectedResult, $result);
+    }
+
+    protected function tearDown()
+    {
+        $sql = "DELETE FROM `MemberData` WHERE `memberName` = 'Lid_Chen'" ;
+        $prepare = $this->db->prepare($sql);
+        $prepare->execute();
+        $sql = "DELETE FROM `TransactionDetails` WHERE `memberName` = 'Lid_Chen'" ;
+        $prepare = $this->db->prepare($sql);
+        $prepare->execute();
     }
 }
 
